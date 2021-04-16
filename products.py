9199411920b8f4dd100ac03a9ccb9a46d41de84a
Leon Lin <qqqq12368@gmail.com>
2021-04-16 21:10:03 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #繼續
+		name, price = line.strip().split(',')  #strip去掉多餘空格或換行符號; split使用逗點做切割
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -12,7 +22,15 @@ while True:
 	products.append([name, price])
 print(products)
 
-
+# 印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+# 寫入檔案
+with open('products.csv', 'w') as f:  		#with 就是自動close; encoding最廣泛使用編碼，避免亂碼
+	f.write('商品,價格\n')  					#欄位名稱
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')   #csv檔都會用逗點來換格
+											# int為轉整數 , str為轉字串
+
 
